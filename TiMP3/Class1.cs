506341a8e76eb3a0ed1924f80002f524ee66d536
KalyanovK <62464446+KalyanovK@users.noreply.github.com>
2020-07-05 01:40:02 +0300

    Add files via upload

---
+++ TiMP3/Class1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TiMP3
+{
+    class Class1
+    {
+        
+        public static void sortvstavkami(int[] mas) //вставками
+        {
+
+            Console.WriteLine("Сортировка вставками");
+            int[] result = new int[mas.Length];
+            for (int i = 0; i < mas.Length; i++)
+            {
+                int j = i;
+                while (j > 0 && result[j - 1] > mas[i])
+                {
+                    result[j] = result[j - 1];
+                    j--;
+                }
+                result[j] = mas[i];
+            }
+
+            Console.WriteLine("Сортированный массив");
+            for (int i = 0; i < mas.Length; i++)
+            {
+                Console.Write(result[i] + " ");
+            }
+            Console.WriteLine();
+        }
+        public static void sortviborom(int[] mas) //Выбором
+        {
+            Console.WriteLine("Сортировка выбором");
+            for (int i = 0; i < mas.Length - 1; i++)
+            {
+                int min_i = i;
+                for (int j = i + 1; j < mas.Length; j++)
+                {
+                    if (mas[j] < mas[min_i])
+                    {
+                        min_i = j;
+                    }
+                }
+                int temp = mas[i];
+                mas[i] = mas[min_i];
+                mas[min_i] = temp;
+            }
+            Console.WriteLine("Сортированный массив");
+            for (int i = 0; i < mas.Length; i++)
+            {
+                Console.Write(mas[i] + " ");
+            }
+            Console.WriteLine();
+        }
+        public static void sortshella(int[] vector) //Шелл
+        {
+
+            Console.WriteLine("Сортировка Шелла");
+            int step = vector.Length / 2;
+            while (step > 0)
+            {
+                int i, j;
+                for (i = step; i < vector.Length; i++)
+                {
+                    int value = vector[i];
+                    for (j = i - step; (j >= 0) && (vector[j] > value); j -= step)
+                    {
+                        vector[j + step] = vector[j];
+
+                    }
+                    vector[j + step] = value;
+                }
+                step /= 2;
+
+            }
+            Console.WriteLine("Сортированный массив");
+            for (int i = 0; i < vector.Length; i++)
+            {
+                Console.Write(vector[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+    
+
---
+++ TiMP3/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace TiMP3
+{
+    class Program
+    {
+            static void Main(string[] args)
+            {
+            int c = 1000;
+            int[] mas = new int[c];
+            Random r = new Random();
+            for (int i = 0; i < c; i++)
+            {
+                mas[i] = r.Next();
+            }
+            Console.WriteLine("Несортированный массив");
+            for (int i = 0; i < c; i++)
+            {
+                Console.Write(mas[i] + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            Stopwatch stopwatch = new Stopwatch();
+            Stopwatch stopwatch1 = new Stopwatch();
+            Stopwatch stopwatch2 = new Stopwatch();
+
+            stopwatch.Start();
+            Class1.sortvstavkami(mas);
+            Console.WriteLine();
+            stopwatch.Stop();
+            TimeSpan ts = stopwatch.Elapsed;
+
+            stopwatch1.Start();
+            Class1.sortviborom(mas);
+            Console.WriteLine();
+            stopwatch1.Stop();
+            TimeSpan ts1 = stopwatch1.Elapsed;
+
+            stopwatch2.Start();
+            Class1.sortshella(mas);
+            Console.WriteLine();
+            stopwatch2.Stop();
+            TimeSpan ts2 = stopwatch2.Elapsed;
+
+
+            Console.WriteLine("время работы сортировки вставками " + ts);
+            Console.WriteLine("время работы сортировки выбора " + ts1);
+            Console.WriteLine("время работы сортировки Шелла " + ts2);
+            Console.ReadLine();
+        }
+
+    }
+        
+    
+}
+
